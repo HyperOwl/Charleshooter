f91ad9b7b21af9eaf0c3f816c5fc69d68b4514b4
HyperOwl <dduo2011@gmail.com>
2018-07-23 10:20:17 -0700

    Added Basic Movement
    -Camera Tracking
    -SmoothDamp Movement
    -Clamped Camera Position

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    private Vector2 panLimit;
+    [SerializeField]
+    private GameObject trackObject;
+    private Vector2 pos;
+    
+	void Update () {
+        pos.x = Mathf.Clamp(trackObject.transform.position.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(trackObject.transform.position.y, -panLimit.y, panLimit.y);
+        transform.position = new Vector3(pos.x,pos.y,transform.position.z);
+	}
+}
