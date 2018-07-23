ab1e8f7a62e4fede6d4989c9ab93b2ae5860e077
HyperOwl <dduo2011@gmail.com>
2018-07-23 15:52:10 -0700

    Added wave spawning
    -Added spawning of enemies
    -Now kills enemy when you die

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour {
     private Vector2 pos;
     
 	void Update () {
+        if (!trackObject) return;
         pos.x = Mathf.Clamp(trackObject.transform.position.x, -panLimit.x, panLimit.x);
         pos.y = Mathf.Clamp(trackObject.transform.position.y, -panLimit.y, panLimit.y);
         transform.position = new Vector3(pos.x,pos.y,transform.position.z);
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,10 +1,34 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
-public class EnemyController : MonoBehaviour{ 
-
+public class EnemyController : MonoBehaviour{
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private float speed = 1;
+    //private SpawnManager spawnmanger;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //spawnmanger = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
+        
+    }
+    private void Update()
+    {
+        if (!player) return;
+        Vector2 difference = new Vector2(
+            player.transform.position.x - transform.position.x,
+            player.transform.position.y - transform.position.y);
+        transform.up = difference;
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+   
     public void Die()
     {
+        //spawnmanger.Spawn();
         Destroy(gameObject);
         Debug.Log("MLG PRO");
     }
+
+   
 }
